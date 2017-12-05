ec84628f786e4154a188be1904ea166142f8716b
Cernan Bernardo <cernanb@gmail.com>
2017-12-05 14:43:04 -0800

    Fix syntax error in NotesAdapter

--- notes-js-frontend/src/adapters/notesAdapter.js
+++ notes-js-frontend/src/adapters/notesAdapter.js
@@ -1,20 +1,20 @@
 class NotesAdapter {
-  constructor(){
-    this.baseUrl = 'http://localhost:3000/api/v1/notes'
+  constructor() {
+    this.baseUrl = "http://localhost:3000/api/v1/notes"
   }
 
   getNotes() {
     return fetch(this.baseUrl).then(res => res.json())
   }
 
-  createNote(body){
+  createNote(body) {
     const noteCreateParams = {
-      method: 'POST',
+      method: "POST",
       headers: {
-        'Content-Type':'application/json'
-      }
-    },
-    body: JSON.stringify({body})
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({ body })
+    }
+    return fetch(this.baseUrl, noteCreateParams).then(res => res.json())
   }
-  return fetch(this.baseUrl, noteCreateParams).then(res => res.json())
 }
