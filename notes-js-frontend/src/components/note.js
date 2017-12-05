c9f4aa60c9b4fe8540344a5c88ee0b3509898364
Cernan Bernardo <cernanb@gmail.com>
2017-12-05 14:40:54 -0800

    Build Note model component

---
+++ notes-js-frontend/src/components/note.js
@@ -0,0 +1,14 @@
+class Note {
+  constructor(noteJSON) {
+    this.body = noteJSON.body
+    this.id = noteJSON.id
+  }
+
+  render() {
+    return `<li data-noteid='${this.id}' data-props='${JSON.stringify(
+      this
+    )}' class='note-element'>${
+      this.body
+    } <i data-action='delete-note' class="em em-scream_cat"></i></li>`
+  }
+}
