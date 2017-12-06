0027c662a0b768bc82bc27bf3d0d4f6a9235820f
Cernan Bernardo <cernanb@gmail.com>
2017-12-05 16:21:35 -0800

    Add destroy to routes

--- notes-rails-api/config/routes.rb
+++ notes-rails-api/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :notes, only: [:index, :create]
+      resources :notes, only: [:index, :create, :destroy]
     end
   end
 end
